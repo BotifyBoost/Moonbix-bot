f73cf6ddf27edf112eaea6ae1dd43bc73d723a67
BotifyBoost <cryptomememates@gmail.com>
2024-10-12 04:07:05 +0530

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+
+    REF_LINK: str = "https://t.me/Binance_Moonbix_bot/start?startApp=ref_1874940389&startapp=ref_1874940389&utm_medium=web_share_copy"
+    AUTO_TASK: bool = True
+    AUTO_PLAY_GAME: bool = True
+    DELAY_EACH_ACCOUNT: list[int] = [60, 120]
+
+    MORE_ACCURATE_CAPTCHA_SOLVER: bool = False
+
+    USE_PROXY_FROM_FILE: bool = False
+
+
+settings = Settings()
+
